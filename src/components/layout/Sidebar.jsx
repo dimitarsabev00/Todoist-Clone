3c60d4598ea805946ac8264a467f936285b079a9
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-28 13:51:06 +0300

    Added Logic From Context In Sidebar Component

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -16,32 +16,99 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <ul className="sidebar__generic">
-        <li className={"inbox"}>
-          <span>
-            <FaInbox />
-          </span>
-          <span>Inbox</span>
+        <li
+          className={active === 'inbox' ? 'active' : undefined}
+        >
+          <div
+            aria-label="Show inbox tasks"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive('inbox');
+              setSelectedProject('INBOX');
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setActive('inbox');
+                setSelectedProject('INBOX');
+              }
+            }}
+          >
+            <span>
+              <FaInbox />
+            </span>
+            <span>Inbox</span>
+          </div>
         </li>
-        <li className={"today"}>
-          <span>
-            <FaRegCalendar />
-          </span>
-          <span>Today</span>
+        <li
+          className={active === 'today' ? 'active' : undefined}
+        >
+          <div
+            aria-label="Show today's tasks"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive('today');
+              setSelectedProject('TODAY');
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setActive('today');
+                setSelectedProject('TODAY');
+              }
+            }}
+          >
+            <span>
+              <FaRegCalendar />
+            </span>
+            <span>Today</span>
+          </div>
         </li>
-        <li className={"next_7"}>
-          <span>
-            <FaRegCalendarAlt />
-          </span>
-          <span>Next 7 days</span>
+        <li
+          className={active === 'next_7' ? 'active' : undefined}
+        >
+          <div
+            aria-label="Show tasks for the next 7 days"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive('next_7');
+              setSelectedProject('NEXT_7');
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setActive('next_7');
+                setSelectedProject('NEXT_7');
+              }
+            }}
+          >
+            <span>
+              <FaRegCalendarAlt />
+            </span>
+            <span>Next 7 days</span>
+          </div>
         </li>
       </ul>
-      <div className="sidebar__middle">
+      <div
+        className="sidebar__middle"
+        aria-label="Show/hide projects"
+        onClick={() => setShowProjects(!showProjects)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') setShowProjects(!showProjects);
+        }}
+        role="button"
+        tabIndex={0}
+      >
         <span>
-          <FaChevronDown />
+          <FaChevronDown
+            className={!showProjects ? 'hidden-projects' : undefined}
+          />
         </span>
         <h2>Projects</h2>
       </div>
+
       <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+
       {showProjects && <AddProject />}
     </div>
   );
