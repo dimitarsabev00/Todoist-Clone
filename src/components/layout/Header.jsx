9a2fc0a4815aeba78645ca3020d25d096312e948
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-28 15:03:19 +0300

    Added Quick Add Task Functionality From Plus Button In Header Component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useState } from "react";
 import { FaMoon } from "react-icons/fa";
 
 import Logo from "../../assets/logo.png";
+import AddTask from "../AddTask";
 const Header = ({ darkMode, setDarkMode }) => {
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
   return (
     <header className="header">
       <nav>
@@ -14,7 +18,14 @@ const Header = ({ darkMode, setDarkMode }) => {
         <div className="settings">
           <ul>
             <li className="settings__add">
-              <button aria-label="Quick add task" type="button">
+              <button
+                aria-label="Quick add task"
+                type="button"
+                onClick={() => {
+                  setShowQuickAddTask(true);
+                  setShouldShowMain(true);
+                }}
+              >
                 +
               </button>
             </li>
@@ -30,6 +41,12 @@ const Header = ({ darkMode, setDarkMode }) => {
           </ul>
         </div>
       </nav>
+      <AddTask
+        showAddTaskMain={false}
+        shouldShowMain={shouldShowMain}
+        showQuickAddTask={showQuickAddTask}
+        setShowQuickAddTask={setShowQuickAddTask}
+      />
     </header>
   );
 };
