b7ef9d8c7757d7fe870cff866b80c596875359f9
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-28 05:55:49 +0300

    Added Structure For Layout Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Header from "./components/layout/Header";
+import Content from "./components/layout/Content";
 
 const App = () => {
   return (
     <>
-      <h1>Todoist Clone</h1>
+      <Header />
+      <Content />
     </>
   );
 };
