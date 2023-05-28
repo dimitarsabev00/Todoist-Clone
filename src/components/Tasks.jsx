cee0910f34817e54d61a54a88c1eb909bd7bc9f8
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-28 13:46:14 +0300

    Added Functionality For Tasks Component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,11 +2,36 @@
 import React, { useEffect } from "react";
 import Checkbox from "./Checkbox";
 import { useTasks } from "../hooks/useTasks";
-
+import { collatedTasks } from "../constants";
+import {
+  getTitle,
+  getCollatedTitle,
+  collatedTasksExist,
+} from "../utilities/helpers";
+import { useSelectedProjectValue, useProjectsValue } from "../context";
 const Tasks = () => {
-  const { tasks } = useTasks("1");
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
+
   let projectName = "";
 
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+  }
+
+  if (
+    projects &&
+    projects.length > 0 &&
+    selectedProject &&
+    !collatedTasksExist(selectedProject)
+  ) {
+    projectName = getTitle(projects, selectedProject).name;
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todoist`;
+  });
   return (
     <div className="tasks">
       <h2>{projectName}</h2>
--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,7 +1,14 @@
 import { collatedTasks } from "../constants";
 
+export const getTitle = (projects, projectId) =>
+  projects.find((project) => project.projectId === projectId);
+
+export const getCollatedTitle = (projects, key) =>
+  projects.find((project) => project.key === key);
+
 export const collatedTasksExist = (selectedProject) =>
   collatedTasks.find((task) => task.key === selectedProject);
+
 export const generatePushId = (() => {
   const PUSH_CHARS =
     "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
