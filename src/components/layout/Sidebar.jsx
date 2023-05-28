b692d0274f27cbf10a5c520a063eb94e4a9a8890
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-28 08:00:18 +0300

    Added Content & Styles For Sidebar Component

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,8 +1,44 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li data-testid="inbox" className={"inbox"}>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li data-testid="today" className={"today"}>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li data-testid="next_7" className={"next_7"}>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 days</span>
+        </li>
+      </ul>
+      <div className="sidebar__middle">
+        <span>
+          <FaChevronDown />
+        </span>
+        <h2>Projects</h2>
+      </div>
+      <ul className="sidebar__projects">Projects her</ul>
+      AddProject Component Here
+    </div>
+  );
 };
 
 export default Sidebar;
