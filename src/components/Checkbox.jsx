8c43148f061773330cb0f93002a61d67cba3693f
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-28 07:01:09 +0300

    Added CheckBox Component

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
+const Checkbox = ({ id, taskDesc }) => {
+  const archiveTask = async () => {
+    try {
+      const taskRef = doc(db, "tasks", id);
+      await updateDoc(taskRef, {
+        archived: true,
+      });
+    } catch (error) {
+      // Handle error
+      console.log(error);
+    }
+  };
+
+  return (
+    <div
+      className="checkbox-holder"
+      data-testid="checkbox-action"
+      onClick={() => archiveTask()}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") archiveTask();
+      }}
+      aria-label={`Mark ${taskDesc} as done?`}
+      role="button"
+      tabIndex={0}
+    >
+      <span className="checkbox" />
+    </div>
+  );
+};
+
+export default Checkbox;
