9b520be1665b26ba63fda08524c394b82305ce85
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-28 07:38:53 +0300

    Added Content &  Styles For Header Component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,40 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { FaMoon } from "react-icons/fa";
 
+import Logo from "../../assets/logo.png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src={Logo} alt="Todoist" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li className="settings__add">
+              <button
+                data-testid="quick-add-task-action"
+                aria-label="Quick add task"
+                type="button"
+              >
+                +
+              </button>
+            </li>
+            <li className="settings__darkmode">
+              <button
+                data-testid="dark-mode-action"
+                aria-label="Darkmode on/off"
+                type="button"
+              >
+                <FaMoon />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
